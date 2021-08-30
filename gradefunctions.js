76a0026311be0ee7c3ad11659fe692e8e527db19
chaimbs <bensultanchaim@gmail.com>
2021-08-30 23:57:59 +0300

    4th JS Exercise 30/08/21 (3)
    Added Part 2 of the solution, finding the median.

--- gradefunctions.js
+++ gradefunctions.js
@@ -9,3 +9,12 @@ function difference(a, b) {
 const lowGrade = grades[0];
 const highGrade = grades[grades.length - 1];
 console.log("Range: ", difference(highGrade, lowGrade));
+//Pt. 2 - The median grade (the grade in the middle)
+const len = grades.length;
+const mid = Math.ceil(len / 2);
+let median;
+if (len % 2 == 0) { //If the amount of grades is even, then we need the half of the sum of both mid-range grades.
+    median = (grades[mid] + grades[mid - 1]) / 2;
+}
+else median = grades[mid - 1];//If the amount of grades is odd, then we just get the median grade.
+console.log("Median: ", median);
